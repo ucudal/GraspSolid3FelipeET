cffaff007c891650a93308a41b3e990a207bdef8
[FelipeET] <felipe.etchevers@correo.ucu.edu.uy>
2022-05-07 22:11:09 -0300

    Comentarios en PrintConsola

--- src/Library/PrintConsola.cs
+++ src/Library/PrintConsola.cs
@@ -11,3 +11,8 @@ namespace Full_GRASP_And_SOLID.Library
         }
     }
 }
+
+/*
+Para no incumplir SRP, elimino la clase AllInOnePrinter, ya que tiene mas de una responsabilidad, por lo
+tanto creo las clases PrintConsola y PrintFile, siendo la superclase IPrinter.
+*/
